231bc485ee309559a358c647bd67a63d77719dc8
CastorLemythe <luka-91@hotmail.fr>
2018-08-04 22:45:15 +0200

    norm

--- libft/libft.h
+++ libft/libft.h
@@ -11,27 +11,14 @@
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-
-/*
-**$================{DEFINE}=================$
-*/
 # define LIBFT_H
 # define BUFF_SIZE 42
 # define X '\n'
-
-/*
-**$================{LIBRARY}================$
-*/
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
 
-/*
-**$================{PROTOTYPE}==============$
-**
-**$==STR==$
-*/
 char	*ft_strnstr(const char *dest, const char *src, size_t len);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 size_t	ft_strlcat(char *dest, const char *src, size_t count);
@@ -49,9 +36,6 @@ char	*ft_strtrim(char const *s);
 char	*ft_strnew(size_t size);
 void	ft_strdel(char **as);
 void	ft_strclr(char *s);
-/*
-**$==DISPLAY==$
-*/
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putchar_fd(char c, int fd);
@@ -60,13 +44,7 @@ void	ft_putendl(char const *s);
 void	ft_putstr(char const *s);
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
-/*
-**$==READ==$
-*/
 int		get_next_line(const int fd, char **line);
-/*
-**$==COUNT==$
-*/
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
@@ -80,9 +58,6 @@ int		ft_isascii(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_isprint(int c);
-/*
-**$==MEM==$
-*/
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
@@ -93,16 +68,10 @@ void	ft_tab_del(int **tab, int size);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
-/*
-**$==FONCTION==$
-*/
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 char	*ft_strmap(char const *s, char (*f)(char));
 void	ft_striter(char *s, void (*f)(char *));
-/*
-**$==CONVERTER==$
-*/
 int		ft_atoi(const char *tab);
 char	*ft_itoa(int n);
 
